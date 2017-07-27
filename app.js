08fdaa7f7f011618f818261ce7daf64a4ab1b2a7
sayed30 <seh31628@uga.edu>
2017-07-27 12:47:30 -0400

    README.md

---
+++ app.js
@@ -0,0 +1,377 @@
+
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var neo4j = require('neo4j-driver');
+var neo4j = require('neo4j-driver').v1;
+var app = express();
+var http = require('http');                                                                                   
+var fs = require('fs');
+var formidable = require("formidable");
+var util = require('util');
+
+var nodemailer = require('nodemailer');
+var hostnameField='';
+var problemField='';
+var priorityField =''; 
+var mailField='';
+var searchField='';
+//var txtFile = "./SearchOutput.txt";
+//var file = new File(txtFile);
+//var access = fs.createWriteStream('./node.access.log');
+//process.stdout.write = process.stderr.write = access.write.bind(access);
+var router = express.Router();
+app.set('views', path.join(__dirname,'views'));
+app.set('view engine','ejs');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static(path.join(__dirname,'public')));
+app.use(router);
+
+
+
+var driver = neo4j.driver('bolt://localhost', neo4j.auth.basic('neo4j', 'Getbig'));
+const session = driver.session();
+
+/*
+ *This function takes a hostname or cluster and creates the node, if it does not exist already.
+ */
+function createCluster(cluster){
+const resultPromise = session.run(    
+				  'MERGE (a:Cluster {name: $name}) RETURN a',
+				  {name: cluster}
+				  );
+
+resultPromise.then(result => {
+	session.close();
+    
+	const singleRecord = result.records[0];
+	const node = singleRecord.get(0);
+
+	console.log(node.properties.name);
+
+	// on application exit:
+	//driver.close();
+    });
+
+}
+/*
+ * This function creates the problem associated with the entered hostname, if it does not exist already.
+ */
+
+function createProblem(Problem){
+    
+    const resultPromise = session.run(
+				     
+                                      'MERGE (n:Problem{name:$problem}) RETURN n',
+                                      {problem:Problem
+				      });
+    //name:cluster;                                                                                                                   
+
+
+    resultPromise.then(result => {
+            session.close();
+
+            const singleRecord = result.records[0];
+            const node = singleRecord.get(0);
+
+            console.log(node.properties.name);
+
+            // on application exit:                                                                                                  \
+                                                                                                                                      
+            //driver.close();                                                                                                        \
+                                                                                                                                      
+        });
+
+}
+
+var logger = fs.createWriteStream('log.json', {
+	flags: 'a' // 'a' means appending (old data will be preserved)
+    });
+function search(searchField){
+    //  file.open("w");
+    session.run('MATCH p =(a { name:$searchField })-[r]->(b) RETURN *;',  {searchField:searchField})
+	//{searchField:searchField}
+	.subscribe(
+		   {
+		       
+		       onNext: function (record) {
+			
+			   var node = record.get('p');
+			   logger.log(node);
+
+
+
+
+		       },
+			   onCompleted: function () {
+			   // Completed!
+			   
+			   session.close();
+		       },
+			   onError: function (error) {
+			   console.log(error);
+		       }
+		   }
+		   );
+       
+}
+
+
+
+/*
+ * This function merges the entered hostname with the problem. 
+ */
+function mergeClusterProblem(Cluster,Problem){
+    
+    const resultPromise = session.run(
+				      'MATCH (n:Cluster{name:$Cluster}),(b:Problem{name:$problem}) CREATE UNIQUE (n)-[r:Problem]->(b)',
+				      {Cluster:hostnameField,problem:Problem});
+
+
+    resultPromise.then(result => {
+            session.close();
+
+            const singleRecord = result.records[0];
+            const node = singleRecord.get(0);
+
+            console.log(node.properties.name);
+
+        });
+}
+
+/*
+ * This function sets the priority of the problem. 
+ */
+
+function SetClusterPriority(Priority,Problem){
+   
+    const resultPromise = session.run(
+                                      'MATCH (n:Problem{name:$problemName}) SET n.Priority =$problem',
+                                      {problem:Priority,problemName:Problem
+                                      });
+    //name:cluster;                                                                                                                                                                  
+
+    // if priority > 3 you get an email(){ }
+
+
+    resultPromise.then(result => {
+            session.close();
+
+            const singleRecord = result.records[0];
+            const node = singleRecord.get(0);
+
+            console.log(node.properties.name);
+
+            // on application exit:                                                                                                  \                                               
+
+            //driver.close();                                                                                                        \                                               
+
+        });
+
+
+
+
+
+
+}
+/*
+ * First thing that is recieved when the server starts listening. 
+ */
+
+app.get('/', function(req,res){
+	//	res.render('form');
+	res.sendFile(__dirname+'/views/index.html');
+	//	if(req.method=='POST'){
+	//     processFormFieldsIndividual(req,res);
+	//	}
+
+
+    });
+/*
+ * Posts the results of the submit button, calls process Fields Function. 
+ */
+
+app.post('/formDone',function(req,res){
+	//	if(req.method.toLowerCase()=='post'){
+	processFormFieldsIndividual(req,res);
+	   //createCluster("5");
+	//mail(); 
+	
+});
+
+app.get('/mail',function(req,res){
+	if(mailField = 'True'){
+	mail();
+	}
+    });
+
+app.get('/button',function(req,res){
+	//res.sendFile(__dirname+'/views/index1.html')
+	res.sendFile(__dirname+'/views/searchForm.html');
+    });
+app.post('/formSearch',function(req,res){
+
+	processFormFieldsSearch(req,res);
+	
+
+
+    });
+
+
+
+/*
+ * Displays the HTML page, first function that is called.
+ */
+function displayForm(res) {
+    fs.readFile('./views/index.html', function (err, data) {
+	    res.writeHead(200, {
+		    'Content-Type': 'text/html',
+			'Content-Length': data.length
+			});
+	    res.write(data);
+	    res.end();
+	});
+}
+
+    function processFormFieldsSearch(req, res) {
+	//Store the data from the fields in your data store.                                                                                                                             
+	//The data store could be a file or database or any other store based                                                                                                            
+	//on your application.                                                                                                                                                           
+	var fields = [];
+	var form = new formidable.IncomingForm();
+	form.on('field', function (field, value) {
+		console.log(field);
+		console.log(value);
+
+
+		fields[field] = value;
+		if(field =='search'){
+		    searchField = value;
+
+		}
+		
+	    });
+	form.on('end', function () {
+		//createCluster(hostnameField);
+		//createProblem(problemField);
+		//mergeClusterProblem(hostnameField,problemField);
+		//SetClusterPriority(priorityField,problemField);
+		//console.log(search(searchField));                                                                                                                                                        
+		search(searchField);
+		res.send(__dirname+'/log.json');
+		//
+		//if(mailField = 'True'){
+		    //      mail();                                                                                                                                                      
+		//	}
+		/*fs.readFile('./node.access.log', function (err, data) {
+			res.writeHead(200, {
+				'Content-Type': 'text/html',
+				    'Content-Length': data.length
+				    });
+			res.write(data);
+			res.end();
+		    });
+		*/
+		/**res.writeHead(200, {
+			'content-type': 'text/plain'
+			    });
+
+		res.write('received the data:\n\n');
+		res.end(util.inspect({
+			    fields: fields
+				}));
+		*/
+		//res.send(helloworld.txt);
+		//res.end(util.inspect({
+		//    fields: fields
+		//	}));
+	    });
+	form.parse(req);
+    }
+
+
+
+
+/*
+ * Process the text boxes associated with the HTML page.
+ */
+function processFormFieldsIndividual(req, res) {
+    //Store the data from the fields in your data store.
+    //The data store could be a file or database or any other store based
+    //on your application.
+    var fields = [];
+    var form = new formidable.IncomingForm();
+    form.on('field', function (field, value) {
+	    console.log(field);
+	    console.log(value);
+	   
+	
+	    fields[field] = value;
+	    if(field =='hostname'){
+	    hostnameField = value;
+	    
+	    }
+	    if(field=='problem'){
+	   
+		problemField = value;
+	    }
+	    if(field ='priority'){
+		priorityField = value;
+
+	    }
+	    if(field='mail'){
+		mailField = value;
+
+	    }
+	});
+
+    form.on('end', function () {
+	    createCluster(hostnameField);
+	    createProblem(problemField);
+	    mergeClusterProblem(hostnameField,problemField);
+	    SetClusterPriority(priorityField,problemField);
+	    //	    search();
+	    if(mailField = 'True'){
+		//	mail();
+	    }
+	    res.writeHead(200, {
+		    'content-type': 'text/plain'
+			});
+	    res.write('received the data:\n\n');
+	    res.end(util.inspect({
+			fields: fields
+			    }));
+	});
+    form.parse(req);
+}
+function mail(){
+    var transporter = nodemailer.createTransport('smtps://email@gmail.com:password@smtp.gmail.com'
+    );
+var mailOptions = {
+    from: '',
+    to: '',
+    subject: 'Sending Email using Node.js',
+    text: ''
+};
+//if(priorityField > 3){
+transporter.sendMail(mailOptions, function(error, info){
+	if (error) {
+	    console.log(error);
+	} else {
+	    console.log('Email sent: ' + info.response);
+	}
+    });
+//}
+}
+
+
+
+app.listen(3000);
+console.log('Server Started on port 3000');
+module.exports = app;
+
+
